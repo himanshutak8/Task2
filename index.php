f1bf6b46b84f3df68cdfdc6aa6c925e8da4fdd88
Himanshu <himanshutak18@gmail.com>
2020-05-13 02:15:18 +0530

    dev1 d3

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 print "Hello World";
 print "\n";
 print "Hello Master new Developer here Dev1...!!!";
+echo "<a href='echo-order-list.php'style='color: red; font-size: 10pt'>";
 print "\n";
 print "Hello Master new Developer here Dev2...!!!";
 ?>
