196b451d3b6f1d44633d1b5e19a7cf498c82fe86
Himanshu <himanshutak18@gmail.com>
2020-05-12 18:07:00 +0530

    dev2 d2

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ print "Hello World";
 print "\n";
 print "Hello Master new Developer here Dev1";
 print "\n";
-print "Hello Master new Developer here Dev2";
+print "Hello Master new Developer here Dev2..!!";
 ?>
 
